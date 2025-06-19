02d7c2d47115297187fb379c648fa6397d07e39e
Moon-fy <yuntepy@gmail.com>
2025-06-19 15:08:39 +0700

    Add files via upload
    This is a professional Calculator

---
+++ cal.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('.value');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(item => {
+    item.onclick = () => {
+        try{
+            if(item.dataset.button === 'C'){
+                display.value = '';
+            } else if(item.dataset.button === 'CE'){
+                let string = display.value;
+                display.value = string.substr(0, string.length -1);
+            } else if(item.dataset.button === '='){
+                if(display.value !== ''){
+                display.value = eval(display.value);
+            }
+            } else{
+                display.value += item.dataset.button;
+            }
+        } catch(error){
+            display.value = 'Invalid Entry';
+            setTimeout( () => (display.value = ''), 1000)
+        }
+    }
+})
